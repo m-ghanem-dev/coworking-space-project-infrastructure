b008d1f381cecf675c9c3cbd46910b40df341e16
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-08-03 00:58:39 +0200

    lock providers

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
 terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
-    bucket = "s3-tf-backend-mydemo"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
+    bucket         = "s3-tf-backend-mydemo"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
   }
 }
