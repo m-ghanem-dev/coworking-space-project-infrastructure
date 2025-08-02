51b56c425c6a4c69c950a20c966778a67c329949
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-08-02 22:36:10 +0200

    spawn eks cluster

---
+++ eks-cluster.tf
@@ -0,0 +1,24 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = var.k8s_version
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.public_subnets
+
+  enable_cluster_creator_admin_permissions    = true
+  cluster_endpoint_public_access              = true
+  cluster_endpoint_private_access             = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      min_capacity     = 1
+      max_capacity     = 3
+
+      instance_types = ["t3.medium"]
+      subnet_ids     = module.vpc.public_subnets
+    }
+  }
+}
