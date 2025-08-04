17342d9efaff717ec42b164b432b29e48c3f5a60
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-08-04 23:46:23 +0200

    fix ecr repo

--- ecr.tf
+++ ecr.tf
@@ -1,15 +1,6 @@
 #######################
 # ECR Repositories
 #######################
-resource "aws_ecr_repository" "frontend" {
-  name                 = "frontend"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
 
 resource "aws_ecr_repository" "backend" {
   name                 = "backend"
